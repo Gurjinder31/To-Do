e4ce70b2c8fdba855dbd1d0314c1503c0fedb26e
Gurjinder Singh <gs@hub.com>
2020-09-17 21:58:18 -0300

    Updates

---
+++ precache-manifest.c9e1fb2f98a2c1ff9d6c08e4e7f08ca5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a101b4e91917946ddaf59f83eaeaad",
+    "url": "/Gurjinder31/React-To-Do-App.git/index.html"
+  },
+  {
+    "revision": "f24faab1df7b5763f502",
+    "url": "/Gurjinder31/React-To-Do-App.git/static/css/main.9cc9d4bc.chunk.css"
+  },
+  {
+    "revision": "1d8824bcb65372f9a2a3",
+    "url": "/Gurjinder31/React-To-Do-App.git/static/js/2.d8cf17e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Gurjinder31/React-To-Do-App.git/static/js/2.d8cf17e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f24faab1df7b5763f502",
+    "url": "/Gurjinder31/React-To-Do-App.git/static/js/main.54a1c0cd.chunk.js"
+  },
+  {
+    "revision": "c0b820e435114ff2e230",
+    "url": "/Gurjinder31/React-To-Do-App.git/static/js/runtime-main.0164ed3f.js"
+  }
+]);
